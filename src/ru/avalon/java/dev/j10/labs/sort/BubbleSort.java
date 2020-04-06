cf744d3f2135770f8d67398bd275352443d36e22
DenisFilichev <denis.filic@gmail.com>
2020-04-06 15:28:34 +0300

    Код исправлен в соответствии с замечаниями

--- src/ru/avalon/java/dev/j10/labs/sort/BubbleSort.java
+++ src/ru/avalon/java/dev/j10/labs/sort/BubbleSort.java
@@ -4,17 +4,16 @@ import ru.avalon.java.dev.j10.labs.Sort;
 public class BubbleSort implements Sort {
 
     public void sort(int[] array) {
-        int meter=1;// переменная для подсчета количества перемещений чисел за одну итерацию цикла while
+        int meter=array.length;// переменная для подсчета количества перемещений чисел за одну итерацию цикла while
         while (meter>0){ // если meter=0, то массив считается отсортированным
-            meter=0; 
             for (int i=1; i<array.length; ++i){
                if (array[i-1]>array[i]){
                     array[i-1] = array[i-1] + array[i];
                     array[i] = array[i-1] - array[i];
                     array[i-1] = array[i-1]-array[i];
-                    meter++;
                 }
             }
+            meter--;
         }
     }
 }
--- src/ru/avalon/java/dev/j10/labs/sort/ShellSort.java
+++ src/ru/avalon/java/dev/j10/labs/sort/ShellSort.java
@@ -6,9 +6,10 @@ public class ShellSort implements Sort {
     public void sort(int[] array) {
 
         int d = array.length; // переменная для определения шага движения
+        int backward;
         while (d>0){
             d/=2; // уменьшаем шаг движения на двое с каждой итерацией цикла while
-            //Сортируем числа удаленные друг от друга на шаг d пузырьковым методом
+            //Сортируем числа удаленные друг от друга на шаг d
             for (int i=0; i+d<array.length;++i){
                 // Производим проверку чисел ввигаясь вперед
                 if (array[i]>array[i+d]){
@@ -16,15 +17,16 @@ public class ShellSort implements Sort {
                     array[i+d] = array[i] - array[i+d];
                     array[i] = array[i]-array[i+d];
                 }
-                // Производим проверку чисел ввигаясь назад
-                for (int backward=i+d; backward-d>=0; backward--){
-                    if (array[backward]<array[backward-d]){
-                    array[backward] = array[backward] + array[backward-d];
-                    array[backward-d] = array[backward] - array[backward-d];
-                    array[backward] = array[backward]-array[backward-d];
-                }
-                }
-            }
-        }
-    }
+                backward=i;
+                if (backward-d>=0){
+                    while (array[backward]<array[backward-d]){
+                        array[backward] = array[backward] + array[backward-d];
+                        array[backward-d] = array[backward] - array[backward-d];
+                        array[backward] = array[backward]-array[backward-d];
+                        backward--;
+                    }//end while
+                }//end if
+            }//end for
+        }//end while
+    }// end sort
 }
